15df13698d0379d5db2f4b60823a327226989bab
lz13 <lovro.zelic@icloud.com>
2022-11-01 16:30:35 +0100

    Fix PayTypeSelector/index.js

--- app/javascript/PayTypeSelector/index.jsx
+++ app/javascript/PayTypeSelector/index.jsx
@@ -7,7 +7,7 @@ class PayTypeSelector extends React.Component {
 			  <label htmlFor="order_pay_type">Pay type</label>
 			  <select id="order_pay_type" name="order[pay_type]">
 			    <option value="">Select a payment method</option>
-			    <option value="Check"><Check</option>
+			    <option value="Check">Check</option>
 			    <option value="Credit card">Credit card</option>
 			    <option value="Purchase order">Purchase order</option>
 			  </select>
