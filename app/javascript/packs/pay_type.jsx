97d5c8e175ab4f028f0888650065dc6f0ddf0445
lz13 <lovro.zelic@icloud.com>
2022-11-01 16:43:29 +0100

    Fix pay_type.jsx

--- app/javascript/packs/pay_type.jsx
+++ app/javascript/packs/pay_type.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import {createRoot} from 'react-dom/client'
 import PayTypeSelector from 'PayTypeSelector'
 
 document.addEventListener('turbolinks:load', function() {
-	var element = document.getElementById("pay-type-component");
-	ReactDOM.render(<PayTypeSelector />, element);
+	const rootElement = document.getElementById("pay-type-component");
+	const root = createRoot(rootElement);
+	
+	root.render(<PayTypeSelector />);	
 });
