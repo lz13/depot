d3cba12fe55f7442ae1180d3b8c6c87aeaf1899f
lz13 <lovro.zelic@icloud.com>
2022-11-03 13:39:17 +0100

    Configure Email settings

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,18 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Mailer configuration.
+Rails.application.configure do
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    user_name: "empujadordecasos@gmail.com",
+    password: "tS@Cec*31yswcOQ7",
+    enable_starttls_auto: true
+  }
+end
