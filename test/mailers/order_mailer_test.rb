33fa960452fb887e69c8272183c4572c40f2ba23
lz13 <lovro.zelic@icloud.com>
2022-11-03 15:04:07 +0100

    Test Mailer

--- test/mailers/order_mailer_test.rb
+++ test/mailers/order_mailer_test.rb
@@ -2,19 +2,19 @@ require 'test_helper'
 
 class OrderMailerTest < ActionMailer::TestCase
   test "recieved" do
-    mail = OrderMailer.recieved
-    assert_equal "Recieved", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderMailer.recieved(orders(:one))
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal [ENV["GMAIL_USERNAME"]], mail.from
+    assert_match /1 x Programming Ruby 1.9/, mail.body.encoded
   end
 
   test "shipped" do
-    mail = OrderMailer.shipped
-    assert_equal "Shipped", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderMailer.shipped(orders(:one))
+    assert_equal "Pragmatic Store Order Shipped", mail.subject
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal [ENV["GMAIL_USERNAME"]], mail.from
+    assert_match /1 x Programming Ruby 1.9/, mail.body.encoded
   end
 
 end
