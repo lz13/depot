ca26b34584619592cf44970084edef9f0e8e1eac
lz13 <lovro.zelic@icloud.com>
2022-11-07 18:23:36 +0100

    Fix login path

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,10 +2,10 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
   
-#  test "should prompt for login" do
-#    get login_url                     !!! Test is failing here!!!
-#    assert_response :success
-#  end
+  test "should prompt for login" do
+    get login_url
+    assert_response :success
+  end
   
   test "should login" do
     dave = users(:one)
