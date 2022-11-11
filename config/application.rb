5fdbc7303cf5e638ed901c3777419973ae855e8c
lz13 <lovro.zelic@icloud.com>
2022-11-11 18:22:30 +0100

    Add newlines

--- config/application.rb
+++ config/application.rb
@@ -14,14 +14,6 @@ module Depot
     
     config.filter_parameters += [ :credit_card_number ]
 
-    # config.middleware.use I18n::JS::Middleware
-
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml')]
-    # config.i18n.default_locale = :en
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.middleware.use I18n::JS::Middleware
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,13 @@ Rails.application.routes.draw do
     post 'login' => :create
     delete 'logout' => :destroy
   end
-  
   get 'sessions/create'
   get 'sessions/destroy'
 
   resources :users
+  resources :products do
+    get :who_bought, on: :member
+  end
 
   scope '(:locale)' do
     resources :orders
@@ -17,9 +19,4 @@ Rails.application.routes.draw do
     resources :carts
     root 'store#index', as: 'store_index', via: :all
   end
-
-  resources :products do
-    get :who_bought, on: :member
-  end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
