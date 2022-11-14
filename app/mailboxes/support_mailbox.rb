e29ca559468e9a2947e0e902fbd1ac46c5b043d3
lz13 <lovro.zelic@icloud.com>
2022-11-14 19:36:35 +0100

    Use Conductor to send emails locally

--- app/mailboxes/support_mailbox.rb
+++ app/mailboxes/support_mailbox.rb
@@ -1,9 +1,9 @@
 class SupportMailbox < ApplicationMailbox
   def process
     puts "START SupportMailbox#process:"
-    puts "From: #{mail.from_address}"
+    puts "From   : #{mail.from_address}"
     puts "Subject: #{mail.subject}"
-    puts "Body: #{mail.body}"
+    puts "Body   : #{mail.body}"
     puts "END SupportMailbox#process:"
   end
 end
