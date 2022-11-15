7f8fc0762a9e86ce2c05b7694cbfbc7d6b12b571
lz13 <lovro.zelic@icloud.com>
2022-11-15 14:56:45 +0100

    Create UI to see support requests

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,22 @@ Rails.application.routes.draw do
     post 'login' => :create
     delete 'logout' => :destroy
   end
-  
-  get 'sessions/create'
-  get 'sessions/destroy'
+
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
   resources :products do
     get :who_bought, on: :member
   end
+
+  resources :support_requests, only: [:index, :update ]
+  
+  resources :orders
+  resources :line_items
+  resources :carts
+  
+  root 'store#index', as: 'store_index'
+
+  get 'sessions/create'
+  get 'sessions/destroy'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
