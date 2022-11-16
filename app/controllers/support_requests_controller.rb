efc59d5c5afa0de391627f4a5bbbde2d70084a53
lz13 <lovro.zelic@icloud.com>
2022-11-16 13:07:15 +0100

    Respond to Support Requests with rich text

--- app/controllers/support_requests_controller.rb
+++ app/controllers/support_requests_controller.rb
@@ -3,4 +3,10 @@ class SupportRequestsController < ApplicationController
         @support_requests = SupportRequest.all 
     end
 
+    def update
+        support_request = SupportRequest.find(params[:id])
+        support_request.update(response: params.require(:support_request)[:response])
+        SupportRequestMailer.respond(support_request).deliver_now
+        redirect_to support_requests_path
+    end
 end
